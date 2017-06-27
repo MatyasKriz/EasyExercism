cf5483d0b56c9b2e0998eee0c2417338214da28e
Matyáš Kříž <kriz.matyas8.10@gmail.com>
2017-06-27 10:41:07 +0200

    Implement main functionality.

---
+++ EasyExercism.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import sys
+import signal
+import argparse
+import subprocess
+import re
+
+
+def signal_handler(signal, frame):
+    if signal is signal.SIGINT:
+        print('That\'s okay, see you later!')
+        sys.exit(1)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+def get_info(language):
+    bash_command = "exercism fetch {}".format(language)
+    output = subprocess.check_output(['bash', '-c', bash_command])
+
+    project_name = ''
+    path = ''
+    is_new_problem = False
+    for line in output.decode("utf-8").split('\n'):
+        if line.startswith(language):
+            try:
+                project_name = re.search('\(([^\)]+)\)', line).group(1)
+                path = re.search('\/\s*(.*)$', line).group(0)
+            except AttributeError:
+                print('Problem with parsing project name and path to project')
+                sys.exit(2)
+        elif 'new' in line:
+            is_new_problem = True
+
+    if not project_name:
+        print('Couldn\'t find any project')
+        sys.exit(2)
+
+    file_name = project_name.replace(' ', '')
+    return file_name, path, is_new_problem
+
+
+def fetch_problem(language):
+    bash_command = "exercism fetch {}".format(language)
+    subprocess.call(['bash', '-c', bash_command])
+
+    _, path, _ = get_info(language)
+
+    generate_project(language, path)
+
+
+def submit_solution(language, submit_all, suffix):
+    file_name, path, is_new_problem = get_info(language)
+
+    if is_new_problem:
+        print('Detected that \'submit\' was called before \'fetch\'. Cowardly shutting down.')
+        generate_project(language, path)
+        sys.exit(1)
+
+    file_suffixes = {"swift": "swift"}
+    if suffix:
+        file_suffix = suffix
+    elif language in file_suffixes:
+        file_suffix = file_suffixes[language]
+    else:
+        file_suffix = input('What suffix is your language using? ')
+
+    if submit_all:
+        pass
+    else:
+        bash_command = "exercism submit {0}/Sources/{1}.{2}".format(path, file_name, file_suffix)
+        subprocess.call(['bash', '-c', bash_command])
+
+
+def generate_project(language, path):
+    generate_commands = {"swift": "swift package -C {} generate-xcodeproj".format(path)}
+
+    if language in generate_commands:
+        bash_command = generate_commands[language]
+        subprocess.call(['bash', '-c', bash_command])
+
+
+parser = argparse.ArgumentParser(epilog='use -l or --language to give language name to script directly')
+subparsers = parser.add_subparsers(dest='command')
+subparser_fetch = subparsers.add_parser('fetch', help='fetch a new problem or get info about the current one')
+subparser_fetch.add_argument('language', type=str, help='language you wish to use')
+subparser_submit = subparsers.add_parser('submit', help='submit a solution to current problem')
+subparser_submit.add_argument('language', type=str, help='language you wish to use')
+subparser_submit.add_argument('-a', '--allFiles', help='use default name and don\'t ask for a new one', action='store_true')
+subparser_submit.add_argument('-s', '--suffix', help='give file suffix to script directly')
+args = parser.parse_args()
+
+language = ''
+if not hasattr(args, 'language'):
+    language = input('Which language do you wish to exercise today? ')
+else:
+    language = args.language
+
+if 'fetch' in args.command:
+    fetch_problem(language)
+elif 'submit' in args.command:
+    submit_solution(language, args.allFiles, args.suffix)
+else:
+    print('Sorry, I don\'t understand. Try again?\n')
+    parser.print_help()
+    sys.exit(2)
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# EasyExercism
-Fetches and submits Exercism exercises without the need to remember name of every single one.
+# Easy Exercism Script
+
+Easy and straightforward fetching and submitting for problems from exercism.io.
+
+  - Fetch and for some languages automatically generate project files
+  - Submit your solutions without having to look up the name of your file(s)
+  - ???
+  - _Magic_ (you can check the code if you want)
+  - Profit with the time you save and move on to the next problem
+
+## Usage
+
+  - run script with either `fetch` or `submit` arguments (if you don't follow it up with language name, you will be prompted)
+  - two possible ways of executing the script:
+     1. recommended way: add `alias easyexercism=python3 ~/PATH/TO/THE/SCRIPT.py` to your shell startup script
+     2. a different way: `(sudo) chmod a+x ~/PATH/TO/THE/SCRIPT.py` and then creating `alias easyexercism=./PATH/TO/THE/SCRIPT.py`
+
+
+## Dependencies
+  - [exercism CLI](http://exercism.io/clients/cli)
+  - [Python3](https://www.python.org/downloads/)
+
+## Liability
+
+I am not responsible for any unfinished submissions. The script does protect you against writing `submit` before `fetch`, but only once, it's not a babysitter.
+(by the way, yes, theoretically you can just keep using `easyexercism submit LANGUAGE` and it will work exactly the way it should; first time it will fetch a new problem(to get the name, path, etc.) and second time it will submit your code)
+
+### Todo
+ - [ ] Implement possibility to submit all files in Sources folder
+ - [ ] Add more languages' suffixes and project-generation commands
+
+## License
+
+This script is licensed under MIT license.
